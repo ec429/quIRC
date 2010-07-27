35a4235dd02d481b6307e934851ce973de38a5fb
Sound and Fury <ec429@cam.ac.uk>
2010-07-27 03:32:30 +0100

    Fix channel joining

--- quirc.c
+++ quirc.c
@@ -282,6 +282,7 @@ int main(int argc, char *argv[])
 									if(strcmp(src, nick)==0)
 									{
 										printf(CLA "You (%s) have joined %s\n" CLA "\n", src, dest+1);
+										chan=strdup(dest+1);
 									}
 									else
 									{
