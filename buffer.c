6b46e429a35221526f2cf1e5468af5daedae2fab
Sound and Fury <ec429@cam.ac.uk>
2010-07-30 01:58:14 +0100

    Fix: bottom of buffer showing when scrolled right to top

--- buffer.c
+++ buffer.c
@@ -100,7 +100,7 @@ int add_to_buffer(int buf, colour lc, char *lt)
 
 int redraw_buffer(void)
 {
-	int sl = ( bufs[cbuf].filled ? (bufs[cbuf].ptr+bufs[cbuf].nlines-(bufs[cbuf].scroll+height-2))%bufs[cbuf].nlines : max(bufs[cbuf].ptr-(bufs[cbuf].scroll+height-2), 0) );
+	int sl = ( bufs[cbuf].filled ? (bufs[cbuf].ptr+max(bufs[cbuf].nlines-(bufs[cbuf].scroll+height-2), 1))%bufs[cbuf].nlines : max(bufs[cbuf].ptr-(bufs[cbuf].scroll+height-2), 0) );
 	int el = ( bufs[cbuf].filled ? (bufs[cbuf].ptr+bufs[cbuf].nlines-bufs[cbuf].scroll)%bufs[cbuf].nlines : max(bufs[cbuf].ptr-bufs[cbuf].scroll, 0) );
 	int dl=el-sl;
 	if(dl<0) dl+=bufs[cbuf].nlines;
