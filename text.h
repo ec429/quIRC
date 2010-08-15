c8198059b8439a00a8a3e12390ad41b50f64ac17
Sound and Fury <ec429@cam.ac.uk>
2010-08-15 17:00:58 +0100

    Give compiler version in --version (-V) output

--- text.h
+++ text.h
@@ -13,6 +13,7 @@
  Copyright (C) 2010 Edward Cree.\n\
  License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
  This is free software: you are free to change and redistribute it.\n\
- There is NO WARRANTY, to the extent permitted by law.\n", "quirc", VERSION_MAJ, VERSION_MIN, VERSION_REV, VERSION_TXT[0]?"-":"", VERSION_TXT
+ There is NO WARRANTY, to the extent permitted by law.\n\
+ Compiler was %s\n", "quirc", VERSION_MAJ, VERSION_MIN, VERSION_REV, VERSION_TXT[0]?"-":"", VERSION_TXT, CC_VERSION
 
 #define USAGE_MSG "quirc [--width=<cols>] [--height=<rows>] [--maxnicklen=<mnln>] [--mcc=<mcc>]\n\t[--force-redraw=<fred>] [--no-server] [--no-chan] [--check]\n\t[--server=<server>] [--uname=<uname>] [--fname=<fname>] [--nick=<nick>]\n\t[--chan=<chan>] [--port=<port>]\nquirc {-h|--help|-V|--version}\n"
