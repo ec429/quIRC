32d954a371bd1759a982f39d9371a24d98a5e078
Sound and Fury <ec429@cam.ac.uk>
2010-09-01 21:15:58 +0100

    Doc updates (fwc, hts)

--- text.h
+++ text.h
@@ -16,4 +16,4 @@
  There is NO WARRANTY, to the extent permitted by law.\n\
  Compiler was %s\n", "quirc", VERSION_MAJ, VERSION_MIN, VERSION_REV, VERSION_TXT[0]?"-":"", VERSION_TXT, CC_VERSION
 
-#define USAGE_MSG "quirc [--width=<cols>] [--height=<rows>] [--maxnicklen=<mnln>] [--mcc=<mcc>]\n\t[--force-redraw=<fred>] [--no-server] [--no-chan] [--check]\n\t[--server=<server>] [--uname=<uname>] [--fname=<fname>] [--nick=<nick>]\n\t[--chan=<chan>] [--port=<port>] [--[no-]fwc]\nquirc {-h|--help|-V|--version}\n"
+#define USAGE_MSG "quirc [--width=<cols>] [--height=<rows>] [--maxnicklen=<mnln>] [--mcc=<mcc>]\n\t[--force-redraw=<fred>] [--no-server] [--no-chan] [--check]\n\t[--server=<server>] [--uname=<uname>] [--fname=<fname>] [--nick=<nick>]\n\t[--chan=<chan>] [--port=<port>] [--[no-]fwc] [--[no-]hts]\nquirc {-h|--help|-V|--version}\n"
