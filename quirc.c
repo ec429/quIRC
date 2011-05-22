ec429/quIRC

commit fbc2ee13b34a7c08611d820597e6327a617dbb6b

de-bind fd when a tab goes dead; this should fix the 'read on a dead tab' bug

quirc.c
@@ -160,6 +160,7 @@ int main(int argc, char *argv[])
 								if(bufs[b].handle)
 								{
 									close(bufs[b].handle);
+									bufs[b].handle=0; // de-bind fd
 									FD_CLR(bufs[b].handle, &master);
 									add_to_buffer(b, c_err, "Outbound ping timeout", "Disconnected: ");
 								}
@@ -180,6 +181,7 @@ int main(int argc, char *argv[])
 					else if(!bufs[bufs[b].server].live)
 					{
 						bufs[b].live=false;
+						bufs[b].handle=0; // just in case
 						add_to_buffer(b, c_err, "Connection to server lost", "Disconnected: ");
 					}
 				}
@@ -254,6 +256,7 @@ int main(int argc, char *argv[])
 										char emsg[64];
 										sprintf(emsg, "irc_rx(%d, &%p): %d", fd, packet, e);
 										close(fd);
+										bufs[b].handle=0; // de-bind fd
 										FD_CLR(fd, &master);
 										bufs[b].live=false;
 										add_to_buffer(0, c_err, emsg, "error: ");
@@ -345,6 +348,7 @@ int main(int argc, char *argv[])
 									char emsg[32];
 									sprintf(emsg, "buf %d, fd %d", b, fd);
 									close(fd);
+									bufs[b].handle=0; // de-bind fd
 									FD_CLR(fd, &master);
 									bufs[b].live=false;
 									add_to_buffer(0, c_err, emsg, "error: read on a dead tab: ");

buffer.c
@@ -212,6 +212,7 @@ int free_buffer(int buf)
 			{
 				bufs[b].server=0; // orphaned; should not happen
 				bufs[b].live=false;
+				bufs[b].handle=0; // just in case
 			}
 			else if(bufs[b].server>buf)
 			{

irc.c
@@ -204,6 +204,7 @@ int irc_rx(int fd, char ** data)
 				{
 					add_to_buffer(b, c_err, strerror(errno), "irc_rx: recv:");
 					bufs[b].live=false;
+					bufs[b].handle=0; // de-bind fd
 				}
 			}
 			cr=true; // just crash out with a partial message
@@ -793,8 +794,8 @@ int rx_kill(message pkt, int b, fd_set *master)
 			{
 				add_to_buffer(b2, c_quit[0], pkt.nargs<2?"":pkt.args[1], "KILLed: ");
 				bufs[b2].live=false;
+				bufs[b2].handle=0; // de-bind fd
 				bufs[b2].hi_alert=5;
-				bufs[b2].handle=0;
 			}
 		}
 		redraw_buffer();
@@ -845,6 +846,7 @@ int rx_kick(message pkt, int b)
 			{
 				add_to_buffer(b2, c_quit[0], pkt.nargs<3?"(No reason)":pkt.args[2], "Kicked: ");
 				bufs[b2].live=false;
+				bufs[b2].handle=0; // just in case
 				bufs[b2].hi_alert=5;
 			}
 		}
@@ -892,8 +894,8 @@ int rx_error(message pkt, int b, fd_set *master)
 		{
 			e_buf_print(b2, c_quit[0], pkt, "Disconnected: ");
 			bufs[b2].live=false;
+			bufs[b2].handle=0; // de-bind fd
 			bufs[b2].hi_alert=5;
-			bufs[b2].handle=0;
 		}
 	}
 	return(redraw_buffer());

commit 8bd79a867b53f00c72c1448856d224d9758a74dd

Fix quit on select error (regression introduced by rev fbc2ee1)
Also added some ideas to plans

plans
@@ -10,6 +10,12 @@ Fix possible issue with CTCP.  Funny stuff (including random characters) happene
 
 Proper handling of Unicode in character-based things like cursor-movement, backspace.
 
+/set (genconfig produced) for booleans: replace weird things with something more sensible.
+
+Optional rolling ts display on input line.
+
+Improve the parsing of mirc-colour-codes (to fix things like \003124 which currently gets eaten entirely).  The relevant code is in bits.c.
+
 /dump command.  Pipes a screendump (or better still, unprocessed text) into program of choice.
 
 Logging & Persistent backscroll.

quirc.c
@@ -179,8 +179,8 @@ int main(int argc, char *argv[])
 								if(bufs[b].handle)
 								{
 									close(bufs[b].handle);
-									bufs[b].handle=0; // de-bind fd
 									FD_CLR(bufs[b].handle, &master);
+									bufs[b].handle=0; // de-bind fd
 									add_to_buffer(b, c_err, "Outbound ping timeout", "Disconnected: ");
 								}
 								bufs[b].alert=true;
@@ -212,8 +212,7 @@ int main(int argc, char *argv[])
 		readfds=master;
 		if(select(fdmax+1, &readfds, NULL, NULL, &timeout)==-1)
 		{
-			perror("\nselect");
-			state=1;
+			add_to_buffer(0, c_err, strerror(errno), "select: ");
 		}
 		else
 		{